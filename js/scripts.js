0253993ff4e2c19ed27fd8015fc4ff06a514eeef
Brian Nelson <nelsonsbrian@gmail.com>
2018-08-13 16:57:14 -0700

    add initial files and folders

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+function removeDuplicateUsingFilter(inputArrary){
+  let noDuplicates = inputArrary.filter(function(elem, index, self) {
+    return index == self.indexOf(elem);
+  });
+  return noDuplicates
+}
+
+
+$(document).ready(function() {
+
+
+  $("#workOrder").submit(function(event) {
+    event.preventDefault();
+
+      var inputTextArray = [];
+      var inputTemp = $("input#inputText").val();
+      inputTextArray = inputTemp.split(" ");
+
+      var noDuplicates = removeDuplicateUsingFilter(inputTextArray);
+
+
+
+      var original;
+      noDuplicates.forEach(function(uniqueWord) {
+        var count = 0;
+        inputTextArray.forEach(function(duplicate) {
+          if (uniqueWord === duplicate) {
+            count++;
+          }
+
+
+        });
+
+
+        $(".output ul").append("<li>Word: " + uniqueWord + " Count: " + count + "</li>")
+
+      });
+
+
+  });
+});
+
+
+
+
+
+// var noDuplicates = inputTextArray.map(function(word) {
+//   if (word === "this") {
+//     return word;
+//   }
+// });
+//
+// alert(noDuplicates);
