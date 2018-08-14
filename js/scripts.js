6d16b4a4d81b0586607b07d793ad7f2542cb0690
Brian Nelson <nelsonsbrian@gmail.com>
2018-08-13 17:29:06 -0700

    add if/else ladder logic for field validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
 
-  $("button").submit(function(event) {
+  $("#countUpForm").submit(function(event) {
     event.preventDefault();
 
-    var countTo = $("#input1").val();
-    var countBy = $("#input2").val();
 
-    if (isNan(countTo) || isNan(countBy) || countTo === undefined || countBy === undefined) {
+    var countTo = Math.abs(parseInt($("#input1").val()));
+    var countBy = Math.abs(parseInt($("#input2").val()));
+
+    if (isNaN(countTo) || isNaN(countBy) || countTo === undefined || countBy === undefined) {
       alert("Enter a number in both fields.")
+    } else if (countBy > countTo) {
+      alert("Count by number needs to be smaller than count to.")
     } else {
-      $(".output").append("Count to: " + countTo);
-      $(".output").append("Count by:" + countBy);
+      $(".output").append("Count to: " + countTo + "<br>");
+      $(".output").append("Count by: " + countBy + "<br>");
 
       var loops = Math.floor(countTo / countBy);
       var runningCount = 0;
       for (i = 0; i < loops; i++) {
         runningCount += countBy;
         $(".output").append(runningCount + ", ");
-      };
+      }
 
 
 
@@ -25,5 +28,5 @@ $(document).ready(function() {
 
   });
 
-  });
+
 });
